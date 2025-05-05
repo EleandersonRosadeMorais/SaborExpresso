cde9813c7285c242b4284af0e69bd969680e7df6
Eleanderson Rosa de Morais <eleandersonmorais@gmail.com>
2025-05-05 20:39:05 -0300

    Décimo commit: Melhorando ainda mais a parte visual do projeto!

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -3,14 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBj-Ylk49kOBTsIG7W_HbBl0NuplZSqEjU",
+  authDomain: "saborexpresso-b7f6f.firebaseapp.com",
+  databaseURL: "https://saborexpresso-b7f6f-default-rtdb.firebaseio.com",
+  projectId: "saborexpresso-b7f6f",
+  storageBucket: "saborexpresso-b7f6f.firebasestorage.app",
+  messagingSenderId: "635279162003",
+  appId: "1:635279162003:web:fde0c2a20f0131d692eb66",
+  measurementId: "G-BT2FFLQF5X"
 };
 
 const app = initializeApp(firebaseConfig);
